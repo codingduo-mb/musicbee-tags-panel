cf9fb0f5a6525d00de3cd2d117197e1880ce8cdc
mat-st <msteiert@posteo.de>
2024-05-03 15:34:21 +0200

    Refactored logger to wirte time in UTC and make it more efficient

--- Logger.cs
+++ Logger.cs
@@ -1,31 +1,11 @@
-﻿/*
-    Copyright 2013 Phillip Schichtel
-
-    This file is part of LyricsReloaded.
-
-    LyricsReloaded is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    LyricsReloaded is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with LyricsReloaded. If not, see <http://www.gnu.org/licenses/>.
-
-*/
-
-using System;
+﻿using System;
 using System.IO;
 using System.Text;
 using static MusicBeePlugin.Plugin;
 
 namespace MusicBeePlugin
 {
-    public class Logger
+    public class Logger : IDisposable
     {
         private const string LOG_FILE_NAME = "mb_tags-panel.log";
 
@@ -38,25 +18,20 @@ namespace MusicBeePlugin
         {
             this.musicBeeApiInterface = musicBeeApiInterface;
             fileInfo = new FileInfo(GetLogFilePath());
-            writer = null;
-        }
-
-        public FileInfo GetFileInfo()
-        {
-            return fileInfo;
+            writer = new StreamWriter(fileInfo.FullName, true, Encoding.UTF8);
+            writer.AutoFlush = false;
         }
 
         private void Write(string type, string message, params object[] args)
         {
-            if (writer == null)
-            {
-                writer = new StreamWriter(fileInfo.FullName, true, Encoding.UTF8);
-                writer.AutoFlush = false;
-            }
-
-            DateTime localTime = DateTime.Now;
-            writer.WriteLine($"{localTime:dd/MM/yyyy HH:mm:ss} [{type.ToUpper()}] {string.Format(message, args)}");
+            DateTime utcTime = DateTime.UtcNow;
+            writer.WriteLine($"{utcTime:dd/MM/yyyy HH:mm:ss} [{type.ToUpper()}] {message}", args);
             writer.Flush();
+        }
+
+        public void Dispose()
+        {
+            writer?.Dispose();
         }
 
         public void Close()
