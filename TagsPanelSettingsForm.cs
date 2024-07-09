af40b7a520d220deab639be1eacedd36c51c8537
kn9ff <msteiert@posteo.de>
2024-07-09 23:23:24 +0200

    Refactored TagsPanelSettingsForm

--- TagsPanelSettingsForm.cs
+++ TagsPanelSettingsForm.cs
@@ -8,8 +8,13 @@ namespace MusicBeePlugin
 {
     public partial class TagsPanelSettingsForm : Form
     {
-        private const string GITHUBLINK = "https://github.com/mat-st/musicbee-tags-panel";
-        private const string TOOLTIPADDTAGPAGE = "Add & select a new tag and a new tabpage";
+        private const string GitHubLink = "https://github.com/mat-st/musicbee-tags-panel";
+        private const string TooltipAddTagPage = "Add & select a new tag and a new tabpage";
+        private const string TagExistsWarning = "Tag exists already";
+        private const string RemoveTagPageWarning = "This will remove the current tag page and you will lose your current tag list. Continue?";
+        private const string WarningCaption = "Warning";
+        private const string AboutMessage = "Tags-Panel Plugin \nVersion {0}\nVisit us on GitHub";
+        private const string AboutCaption = "About Tags-Panel Plugin";
 
         private Dictionary<string, TagsPanelSettingsPanel> tagPanels = new Dictionary<string, TagsPanelSettingsPanel>();
         public SettingsStorage SettingsStorage { get; set; }
@@ -25,50 +30,48 @@ namespace MusicBeePlugin
             VersionLbl.Text = $"Version: {System.Reflection.Assembly.GetExecutingAssembly().GetName().Version}";
             VersionLbl.ForeColor = Color.Black;
 
-            foreach (TagsStorage storage in SettingsStorage.TagsStorages.Values)
+            foreach (var storage in SettingsStorage.TagsStorages.Values)
             {
                 AddPanel(storage);
             }
 
-            toolTipAddTagPage.SetToolTip(btnAddTabPage, TOOLTIPADDTAGPAGE);
+            toolTipAddTagPage.SetToolTip(btnAddTabPage, TooltipAddTagPage);
         }
 
         private void AddPanel(TagsStorage storage)
         {
-            string tagName = storage.GetTagName();
+            var tagName = storage.GetTagName();
             if (tagPanels.ContainsKey(tagName))
             {
-                throw new ArgumentException("This Metadata Type was already added");
+                ShowMessageBox($"This Metadata Type was already added", TagExistsWarning, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
             }
 
-            TagsPanelSettingsPanel tagsPanelSettingsPanel = new TagsPanelSettingsPanel(tagName);
+            var tagsPanelSettingsPanel = new TagsPanelSettingsPanel(tagName);
             tagPanels[tagName] = tagsPanelSettingsPanel;
-            TabPage tabPage = new TabPage(tagName);
-            tabPage.Controls.Add(tagsPanelSettingsPanel);
+            var tabPage = new TabPage(tagName) { Controls = { tagsPanelSettingsPanel } };
             tabControlSettings.TabPages.Add(tabPage);
             tagsPanelSettingsPanel.SetUpPanelForFirstUse();
         }
 
         private void Btn_AddTagPage_Click(object sender, EventArgs e)
         {
-            List<string> usedTags = tagPanels.Keys.ToList();
-            using (TabPageSelectorForm form = new TabPageSelectorForm(usedTags))
+            var usedTags = tagPanels.Keys.ToList();
+            using (var form = new TabPageSelectorForm(usedTags))
             {
-                DialogResult result = form.ShowDialog(this);
+                var result = form.ShowDialog(this);
                 if (result == DialogResult.OK)
                 {
-                    TagsStorage storage = new TagsStorage();
-                    storage.MetaDataType = form.GetMetaDataType();
+                    var storage = new TagsStorage { MetaDataType = form.GetMetaDataType() };
                     if (storage.MetaDataType != null)
                     {
                         try
                         {
                             AddPanel(storage);
-                            form.Close();
                         }
                         catch (ArgumentException ex)
                         {
-                            MessageBox.Show(ex.Message, "Tag exists already", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            ShowMessageBox(ex.Message, TagExistsWarning, MessageBoxButtons.OK, MessageBoxIcon.Warning);
                         }
                     }
                 }
@@ -77,13 +80,13 @@ namespace MusicBeePlugin
 
         private void BtnRemoveTagPage_Click(object sender, EventArgs e)
         {
-            TabPage tabToRemove = tabControlSettings.SelectedTab;
+            var tabToRemove = tabControlSettings.SelectedTab;
             if (tabToRemove != null)
             {
-                DialogResult dialogResult = MessageBox.Show("This will remove the current tag page and you will lose your current tag list. Continue?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                var dialogResult = MessageBox.Show(RemoveTagPageWarning, WarningCaption, MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
                 if (dialogResult == DialogResult.Yes)
                 {
-                    string tagName = tabToRemove.Text;
+                    var tagName = tabToRemove.Text;
                     tabControlSettings.TabPages.Remove(tabToRemove);
                     SettingsStorage.RemoveTagStorage(tagName);
                     tagPanels.Remove(tagName);
@@ -93,14 +96,17 @@ namespace MusicBeePlugin
 
         private void LinkAbout_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            MessageBox.Show("Tags-Panel Plugin " + Environment.NewLine + "Version " + VersionLbl.Text + Environment.NewLine +
-                "Visit us on GitHub", "About Tags-Panel Plugin",
-                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            ShowMessageBox(string.Format(AboutMessage, VersionLbl.Text), AboutCaption, MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void LinkGitHub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            System.Diagnostics.Process.Start(GITHUBLINK);
+            System.Diagnostics.Process.Start(GitHubLink);
+        }
+
+        private void ShowMessageBox(string text, string caption, MessageBoxButtons buttons, MessageBoxIcon icon)
+        {
+            MessageBox.Show(text, caption, buttons, icon);
         }
     }
 }
