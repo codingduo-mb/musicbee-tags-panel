9864853083204990989aa2b7eed055ba842be757
kn9ff <msteiert@posteo.de>
2024-07-14 11:53:47 +0200

    Refactored UIManager for clean code

--- UIManager.cs
+++ UIManager.cs
@@ -11,31 +11,17 @@ namespace MusicBeePlugin
         private const SkinElement DefaultSkinElement = SkinElement.SkinTrackAndArtistPanel;
         private const ElementState DefaultElementState = ElementState.ElementStateDefault;
         private readonly MusicBeeApiInterface _mbApiInterface;
-        private Dictionary<int, Color> _colorCache;
+        private readonly Dictionary<int, Color> _colorCache = new Dictionary<int, Color>();
         private Font _defaultFont;
 
         public UIManager(MusicBeeApiInterface mbApiInterface)
         {
             _mbApiInterface = mbApiInterface;
-            _colorCache = new Dictionary<int, Color>();
-        }
-
-        // Replace Tuple with custom struct to improve performance and readability
-        private struct KeyStruct
-        {
-            public SkinElement SkinElement { get; set; }
-            public ElementComponent Component { get; set; }
-
-            public KeyStruct(SkinElement skinElement, ElementComponent component)
-            {
-                SkinElement = skinElement;
-                Component = component;
-            }
         }
 
         private int GetKeyFromArgs(SkinElement skinElement, ElementState elementState, ElementComponent elementComponent)
         {
-            return (int)(((int)skinElement & 0x7F) << 24) | (((int)elementState & 0xFF) << 16) | ((int)elementComponent & 0xFFFF);
+            return ((int)skinElement & 0x7F) << 24 | ((int)elementState & 0xFF) << 16 | (int)elementComponent & 0xFFFF;
         }
 
         public Color GetElementColor(SkinElement skinElement, ElementState elementState, ElementComponent elementComponent)
@@ -46,17 +32,18 @@ namespace MusicBeePlugin
             {
                 int colorValue = _mbApiInterface.Setting_GetSkinElementColour(skinElement, elementState, elementComponent);
                 color = Color.FromArgb(colorValue);
-                _colorCache[key] = color;
+                _colorCache.Add(key, color);
             }
 
             return color;
         }
 
-
         public void StyleControl(Control formControl)
         {
             if (_defaultFont == null)
+            {
                 _defaultFont = _mbApiInterface.Setting_GetDefaultFont();
+            }
 
             formControl.Font = _defaultFont;
             formControl.BackColor = GetElementColor(DefaultSkinElement, DefaultElementState, ElementComponent.ComponentBackground);
